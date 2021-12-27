6060b5cd570ab2a4bf02d1b62696bdc4fbfde793
dldydgns530@gmail.com <dldydgns530@gmail.com>
2021-12-27 14:15:07 +0900

    오브젝트 chapter1

---
+++ 오브젝트/1장 - 객체,설계/Code/Audience.java
@@ -0,0 +1,11 @@
+public class Audience{
+    private Bag bag;
+
+    public Audience(Bad bad){
+        this.bag = bag;
+    }
+
+    public Bag getBad(){
+        return bag;
+    }
+}
---
+++ 오브젝트/1장 - 객체,설계/Code/Invitation.java
@@ -0,0 +1,3 @@
+public class Invitation{
+    private LocalDateTime when;
+}

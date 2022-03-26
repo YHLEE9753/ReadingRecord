1a7e41b21ef41d6db91fcb1113c7250e486a7431
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-03-26 16:59:49 +0900

    item1

---
+++ codes/BookCode/src/EffectiveJava/chapter2/item1/Fruit.java
@@ -0,0 +1,9 @@
+package EffectiveJava.chapter2.item1;
+
+enum Fruit {
+    APPLE(2), BANANA(1);
+
+    private Fruit(int i) {
+
+    }
+}
---
+++ codes/BookCode/src/EffectiveJava/chapter2/item1/Main.java
@@ -0,0 +1,8 @@
+package EffectiveJava.chapter2.item1;
+
+public class Main {
+    public static void main(String[] args) {
+//        Fruit fruit = new Fruit();
+        Fruit apple = Fruit.APPLE;
+    }
+}
